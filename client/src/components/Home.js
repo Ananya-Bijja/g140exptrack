d2e98684ea47b77d851e865f19ec1c14097a11e2
Saivijaya7887 <saivijaya7887@gmail.com>
2024-12-04 12:26:17 +0530

    added background to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,10 +4,12 @@ import HeroSection from './HeroSection';
 import FeatureSection from './FeatureSection';
 // import Carousel from './Carousel';
 import Footer from './Footer';
+// import { backgroundClip } from 'html2canvas/dist/types/css/property-descriptors/background-clip';
+// import { backgroundImage } from 'html2canvas/dist/types/css/property-descriptors/background-image';
 
 const Home = () => {
   const homeStyle = {
-    backgroundImage: `url('/assets/images/background.jpg')`,
+    backgroundImage: `url('/assets/images/home_background.png')`,
     backgroundSize: 'cover', // Makes sure the image covers the entire viewport
     backgroundPosition: 'center', // Center the image
     backgroundRepeat: 'no-repeat', // Prevent image repetition
@@ -31,3 +33,48 @@ const Home = () => {
 };
 
 export default Home;
+
+
+
+
+// import React from 'react';
+// import Navbar from './Navbar';
+// import HeroSection from './HeroSection';
+// import FeatureSection from './FeatureSection';
+// import Footer from './Footer';
+
+// const Home = () => {
+//   return (
+//     <div style={{ position: 'relative', minHeight: '100vh', overflow: 'hidden' }}>
+//       {/* Background Video */}
+//       <video
+//         autoPlay
+//         loop
+//         muted
+//         playsInline
+//         style={{
+//           position: 'absolute',
+//           top: 0,
+//           left: 0,
+//           width: '100%',
+//           height: '100%',
+//           objectFit: 'cover',
+//           zIndex: -1,
+//         }}
+//       >
+//         <source src="/assets/images/home_background.mp4" type="video/mp4" />
+//         Your browser does not support the video tag.
+//       </video>
+
+//       {/* Page Content */}
+//       <div style={{ position: 'relative', zIndex: 1 }}>
+//         <Navbar />
+//         <HeroSection />
+//         <FeatureSection />
+//         <Footer />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
